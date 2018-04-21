97f1eaeb612867f04eff9f48ddd61e856119ac45
Edison Liu <bijiasuo2006@hotmail.com>
2018-04-21 10:27:57 +0800

    review

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,22 @@
  * License URI:http://support.wrapbootstrap.com/
  * File Description: Place here your custom scripts
  */
+
+$('#submit-post-comment').click(function () {
+  var name = $('#name4').val();
+  var subject = $('#subject4').val();
+  var message = $('#message4').val();
+
+  if (name.length > 0 && subject.length > 0 && message.length > 0) {
+    Email.send("customerservice@australiapetindustry.com",
+      "customerservice@australiapetindustry.com",
+      subject,
+      message, {
+        token: "91e674e7-db5c-438a-bf2d-642282b1309e"
+      });
+
+    alert('Thanks for your review!');
+  }
+
+  return false;
+});
