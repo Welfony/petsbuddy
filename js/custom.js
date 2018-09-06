e86e28d707f3ee35ca1b0ab282f0cfe7c5691efc
Yongjie Liu <yongjie@gstdev.com>
2018-09-06 10:47:11 +0800

    Add baidu

--- js/custom.js
+++ js/custom.js
@@ -8,18 +8,16 @@
  * File Description: Place here your custom scripts
  */
 
-$('#submit-post-comment').click(function () {
+$('#submit-post-comment').click(function() {
   var name = $('#name4').val();
   var subject = $('#subject4').val();
   var message = $('#message4').val();
 
   if (name.length > 0 && subject.length > 0 && message.length > 0) {
-    Email.send("customerservice@australiapetindustry.com",
-      "customerservice@australiapetindustry.com",
-      subject,
-      message, {
-        token: "91e674e7-db5c-438a-bf2d-642282b1309e"
-      });
+    Email.send(
+        'customerservice@australiapetindustry.com',
+        'customerservice@australiapetindustry.com', subject, message,
+        {token: '91e674e7-db5c-438a-bf2d-642282b1309e'});
 
     alert('Thanks for your review!');
     location.reload();
@@ -27,3 +25,11 @@ $('#submit-post-comment').click(function () {
 
   return false;
 });
+
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement('script');
+hm.src = 'https://hm.baidu.com/hm.js?98f5111831c175c78f0e54e7af2b63e8';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(hm, s);
+})();
